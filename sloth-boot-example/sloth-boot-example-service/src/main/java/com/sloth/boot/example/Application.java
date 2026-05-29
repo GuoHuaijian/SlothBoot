205779046959d79f6f8c325246161925fa816f66
guohuaijian <564559079@qq.com>
2026-05-29 21:56:41 +0800

    fix(example): add @MapperScan to Application for mapper bean registration

--- sloth-boot-example/sloth-boot-example-service/src/main/java/com/sloth/boot/example/Application.java
+++ sloth-boot-example/sloth-boot-example-service/src/main/java/com/sloth/boot/example/Application.java
@@ -1,5 +1,6 @@
 package com.sloth.boot.example;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @since 1.0.0
  */
 @SpringBootApplication(scanBasePackages = Application.BASE_PACKAGE)
+@MapperScan("com.sloth.boot.example.domain.mapper")
 public class Application {
 
     /**
